2a0a907e2491580fc4a93dd7f68319cd23afa2cb
Amrendra <amrendramaurya@cedcommerce.com>
2022-05-07 16:40:38 +0530

    boot

---
+++ src/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+<body>
+    <div class="main">
+<nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container-fluid">
+  <a class="navbar-brand col-sm-6 p-3 " style="font-size:1.5rem;" href="#" > Jevelin</a> 
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button> -->
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Home
+          </a>          
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Blog
+          </a>          
+        </li>
+        <li class="nav-item ">
+        <a class="nav-link text-dark" href="#">Portfolio</a>
+      </li>
+        <li class="nav-item col-sm-1.9">
+          <a class="nav-link text-primary dropdown-toggle "  href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Shop<span class="position-absolute top-40 start-15 translate-middle badge rounded-pill bg-danger "style="font-size:.5rem;">
+    New
+    </span>
+          </a>          
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Variefied
+          </a>          
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-dull" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Elements
+          </a>          
+        </li>
+         <li class="nav-item">  
+         <a class="nav-link " href="#"> <i class="bi bi-cart"><span class="position-absolute top-40 start-15 translate-middle badge rounded-pill bg-primary "style="font-size:.5rem;">
+    5 </span></i></a>    
+       </li>
+         <li class="nav-item">
+         <a class="nav-link" href="#"> <i class="bi bi-search"></i></a>    
+         </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+<!-- Default dropend button -->
+
+<div class="btn-group dropend ">
+  <button type="button" class="btn btn-primary dropdown-toggle col-sm-1" data-bs-toggle="dropdown" aria-expanded="false">
+    Home
+  </button>
+  <button type="button" class="btn btn-primary dropdown-toggle col-sm-1" data-bs-toggle="dropdown" aria-expanded="false">
+    Shop
+  </button>
+  <button type="button" class="btn btn-primary dropdown-toggle col-sm-2.5" data-bs-toggle="dropdown" aria-expanded="false">
+    Android phone
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+
+
+</div>
+</body>
+</html>
