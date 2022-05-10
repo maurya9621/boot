59d15d5fefe219172cab5c4ccdd36baba1931358
Amrendra <amrendramaurya@cedcommerce.com>
2022-05-10 11:32:38 +0530

    product_page_bootstrap

--- src/index.php
+++ src/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -15,16 +16,90 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+  .nav-link {
+    padding: 0.5rem 0.4rem;
+  }
+  .navbar {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  hr{
+    margin: 0;
+  }
+  dl, ol, ul {
+    margin-top: 0;
+    margin-bottom: 3rem;
+}
+.text_container{
+  float: left;
+  margin-left: 2.5rem;
+  margin-top: 1rem;
+}
+.image_container{
+  float: left;
+  margin-left: 0px;
+}
+.checked {
+  color: yellow;
+}
+::placeholder {
+  color: white;
+}
+.icon-grey{
+  color: #eeeeee;
+}
+.color{
+  color: lightblue;
+  background-color: #1835cb;
+  color: white;
+}
+.btn{
+ background-color: #f2f2f2;
+}
+.phone {
+  color: #2c4fff;
+}
+.phone1 {
+  border-bottom: 3.2px solid #2c4fff;
+  padding: 1.7rem;
+  color: #2c4fff;
+}
+.hr {
+       border: 2px solid black;
+    }
+    small {
+     margin-left: 3rem;
+     padding-top: 0px;
+    }
+ .icon2-color{
+      color: #2c4fff; 
+ }
+ .text-white{
+  font-size: 1.3rem;
+ }
+ .btn {
+   float: left;
+   background-color: #2c4fff;
+   color: white;
+ }
+ .mail {
+   background-color: grey;
+   color: white;
+ }
+ .send {
+  background-color: grey;
+   color: white;
+ }
+</style>
 <body>
     <div class="main">
 <nav class="navbar navbar-expand-lg navbar-light ">
   <div class="container-fluid">
   <a class="navbar-brand col-sm-6 p-3 " style="font-size:1.5rem;" href="#" > Jevelin</a> 
-    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> -->
     <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+      <ul class="navbar-nav ">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Home
@@ -62,37 +137,177 @@
          <li class="nav-item">
          <a class="nav-link" href="#"> <i class="bi bi-search"></i></a>    
          </li>
-      </ul>
-    </div>
+      </ul>  
+    </div> 
   </div>
-</nav>
-
-
-
-
-
-
+</nav>  <hr>
 
 <!-- Default dropend button -->
+    <ul class="navbar navbar-expand-lg navbar-light nav-item dropend"> 
+          <a class="nav-link  text-muted" href="#"  role="button"  aria-expanded="false">
+           Home  >
+          </a>          
+          <a class="nav-link text-muted" href="#"  role="button"  aria-expanded="false">
+           Shop >
+          </a>          
+          <a class="nav-link  text-dark" href="#" role="button"  aria-expanded="false">
+           <b>Android Phone > </b> 
+          </a>             
+    </ul>
+   
+  <!-- </div> -->
+   
+  <!-- main container starts from here -->
+  <div class="container">
+     <!-- image container starts from here -->
+  <div class="image_container">           
+  <img src="iphone.jpg" class="img-rounded" alt="Cinque Terre" width="504" height="436"> 
+  </div>
 
-<div class="btn-group dropend ">
-  <button type="button" class="btn btn-primary dropdown-toggle col-sm-1" data-bs-toggle="dropdown" aria-expanded="false">
-    Home
-  </button>
-  <button type="button" class="btn btn-primary dropdown-toggle col-sm-1" data-bs-toggle="dropdown" aria-expanded="false">
-    Shop
-  </button>
-  <button type="button" class="btn btn-primary dropdown-toggle col-sm-2.5" data-bs-toggle="dropdown" aria-expanded="false">
-    Android phone
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
+     <!-- text container starts from here -->
+  <div class="text_container">
+  <h2>Android &nbsp;Phone</h2> 
+<span><span class="text-muted" ><del> $199.0</del></span> &nbsp;&nbsp; <big><b>$179.0</b></big> </span> 
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span > (1 review)</span> <br><br>
+<p>
+folly words widow one down few age every seven. if miss part by <br>
+fact he park just shew. Descovered had get considerd projection <br>
+who favorable. necessary up knowledge it tolerably. </p>
+
+
+
+<span><i class="bi bi-arrow-down-circle-fill fa-2x icon-grey "></i></span> &nbsp;&nbsp;&nbsp;
+<span> <big><b>1</b></big> </span>&nbsp;&nbsp;&nbsp;
+<span><i class="bi bi-arrow-up-circle-fill fa-2x icon-grey"></i></span>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<span><button class="color" ><i class="bi bi-cart "></i> &nbsp;&nbsp; Add to cart </button></span> <br> <br>
+<span> <b>SKU:</b> &nbsp;&nbsp;12 </span> <br>
+<span><b>Category:</b> <span class="phone">&nbsp;&nbsp; <b>Phones</b>  </span></span> <br>
+<span><b>Tag:</b>&nbsp;&nbsp; Screen </span> <br> <br>
+<span><button class="btn"><i class="bi bi-share"></i>&nbsp;&nbsp; Share </button></span>  <br><br>
+   <span class="phone1"> <b>Description </b>  </span>
+   <span>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>Reviews(1)</b>  </span> <br><br> <hr class="hr"> <br> <br>
+   <p> Unwiling departure is be dashwoods are an. Use off aggreable law <br>
+       unwiling sir deficient curiosity instantly. Easy mind life fact with see has bore <br>
+       ten. Parish any chatty can elinor direct for former. up as meant widow equal an <br>
+       share last. 
+  </p> <br><br><br><br><br>
+  </div> 
+  
+  <!-- <div class="container"> -->
+  <div class="row">
+    <div class="col-sm-1">
+  </div>
+  <div class="col-sm-4">  
+  <span><i class="bi bi-cart4 fa-2x icon2-color"></i></span>&nbsp;&nbsp;&nbsp;&nbsp;
+       <b> free shipping </b>  
+      <p> <small>  for purchase over $50</small></p>
+    </div>
+
+    <div class="col-sm-4">
+    <span><i class="bi bi-shield-x fa-2x icon2-color"></i></span>&nbsp;&nbsp;&nbsp;&nbsp;
+       <b> free shipping </b>  
+       <p> <small>  for purchase over $50</small> </p>
+       </div>
+    <div class="col-sm-3">
+    <span><i class="bi bi-lightning-charge fa-2x icon2-color"></i></span>&nbsp;&nbsp;&nbsp;&nbsp;
+       <b> free shipping </b>  
+      <p>  <small>  for purchase over $50</small> </p>
+       </div>
+    </div>
+  </div>
+
+  <div class="footer "> 
+
+<div class="container-fluid bg-dark "><br> <br>
+<div class="row">
+<div class="col-sm-1">
+</div>
+<div class="col-sm-2">
+  <span class="text-white"> <h1> Jevelin </h1> </span>
+</div>
+<div class="col-sm-6">
+</div>
+<div class="col-sm-3 ">
+  <span class="text-white"><i class="bi bi-twitter fa-1x"></i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <span class="text-white"><i class="bi bi-instagram fa-1x"></i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <span class=" text-white"><i class="bi bi-youtube fa-1x"></i></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <span class=" text-white"><i class="bi bi-whatsapp fa-1x"></i></span>
+</div>
+</div> <br> <br> <br> <hr class="text-secondary"> <br>  <br>
+
+<div class="footer2">
+<div class="row">
+<div class="col-sm-1">
+</div>
+<div class="col-md">
+<span class="text-white"> <h4> Start Crafting <br> Epic <b>Website</b>  <br> Today! </h4></span>
+<button class="btn">Purchase them</button> 
+</div>
+<div class="col-md">
+  <span class="text-white"> <h4> Useful </h4> </span>
+  <p class="text-white text-size"style="font-size:15px;"> small Documentation intro <br> video showcase <br> buy them </p>
+</div>
+<div class="col-md">
+  <span class="text-white"> <h4> Keep in touch </h4></span>
+  <p class="text-white text-size" style="font-size:15px;"> Nippon. Osaka, Crimson street 78-42 <br> (10am-6pm EST, Mon-Fri) <br> support@brand.com </p>
+</div>
+<div class="col-md">
+  <span class="text-white"> <h4> Sign up for newsletter </h4>  </span>
+  <input class="col-7 border-dark text-white  bg-secondary" type="email" placeholder="email address" >
+   &nbsp;&nbsp;<button class="col-3 bg-secondary text-white border-"dark style="font-size: 19px color:white;";> send</button></p>
+  <p class="text-white text-size" style="font-size: 10px;">&nbsp;&nbsp;&nbsp; *information is not disclose to third party </p>
+</div>
+  </div>
+  </div> <br> <br><br>
+  <div class="row">
+  <div class="col-sm-1"> </div>
+    <div class="col-sm-5">
+  <p class="text-white text-size" style="font-size: 11px;"> <b> @2021 Jevelin, Crafted by Shufflehound </b></p>
+  </div>
+  <div class="col-sm-4"> </div>
+  <div class="col-sm-2">
+  <p class="text-white text-size" style="font-size: 10px;"> <b> Privacy policy &nbsp;&nbsp;&nbsp; Terms & Conditions </b></p>
+  </div>
 </div>
 
 
+ 
 </div>
+  
+ 
+
+
+ 
+
+
+</div >
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+  
+
 </body>
 </html>
